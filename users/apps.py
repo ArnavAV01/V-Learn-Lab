9b267d963433fdcc12467a0872dd5103dc7ab510
arnav <arnav12verma@gmail.com>
2024-10-31 12:45:41 +0530

    Removed previous files and added a new folder name users

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FacultyConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Faculty'
+    name = 'users'
---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_faculty = models.BooleanField(default = False)
+    is_student = models.BooleanField(default = False)
+    
